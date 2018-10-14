f63c505fe29aff05fbc1e5f958884ae24f6e0dfb
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-10-14 11:51:20 +0900

    Merge webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import * as React from 'react'
 
 export default class App extends React.Component {
   public clickButton = () => {
-    console.info('hello click button')
+    console.info('Hello, my name is App')
   }
 
   public render() {
     return (
       <div>
         <h1>React SSR Sample</h1>
+        <p>Hi, How are you??</p>
         <button onClick={this.clickButton}>Hi!</button>
       </div>
     )
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const merge = require("webpack-merge");
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+const base = {
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  }
+};
+
+const serverConfig = merge(base, {
+  target: "node",
+  entry: { server: "./src/server.tsx" },
+  externals: [nodeExternals()]
+});
+
+const clientConfig = merge(base, {
+  target: "web",
+  entry: { client: "./src/client.tsx" }
+});
+
+module.exports = [serverConfig, clientConfig];
