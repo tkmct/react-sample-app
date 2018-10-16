c14c3379b86a6eb9bd807a3795345b25f4c8dfde
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-10-16 22:50:47 +0900

    Remove test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,3 @@
-import * as React from 'react'
-import { create } from 'react-test-renderer'
-import App from '../src/client/App'
-
 test('SnapShot', () => {
-  const tree = create(<App />).toJSON()
-  expect(tree).toMatchSnapshot()
+  pending()
 })
