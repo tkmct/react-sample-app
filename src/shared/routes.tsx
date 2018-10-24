b7a984a58042fddab93a208e69316b54ce411967
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-10-24 14:38:11 +0900

    Add webpackChunkName inline comment

--- src/shared/routes.tsx
+++ src/shared/routes.tsx
@@ -1,9 +1,17 @@
 import loadable from 'loadable-components'
 
-const Home = loadable(() => import('./Home/components/Home'))
-const About = loadable(() => import('./About/components/About'))
-const Counter = loadable(() => import('./Counter/components/Counter'))
-const NotFound = loadable(() => import('./components/NotFound'))
+const Home = loadable(() =>
+  import(/* webpackChunkName: "Home" */ './Home/components/Home')
+)
+const About = loadable(() =>
+  import(/* webpackChunkName: "About" */ './About/components/About')
+)
+const Counter = loadable(() =>
+  import(/* webpackChunkName: "Counter" */ './Counter/components/Counter')
+)
+const NotFound = loadable(() =>
+  import(/* webpackChunkName: "NotFound" */ './components/NotFound')
+)
 
 const routes = [
   {
