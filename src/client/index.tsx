837d28c1c8c9cfe1a4b7786b7fd145780abc2e05
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-14 19:05:45 +0900

    Add sw file

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -39,3 +39,17 @@ if (module.hot) {
     )
   })
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/public/serviceWorker.js')
+    .then(registration => {
+      console.log(
+        'ServiceWorker registration successful with scope: ',
+        registration.scope
+      )
+    })
+    .catch(err => {
+      console.log('ServiceWorker registration failed: ', err)
+    })
+}
