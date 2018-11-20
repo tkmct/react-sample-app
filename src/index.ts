5625b7fb5bf6d2f93f95bdccff3e6e56404ffaf7
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-20 19:46:38 +0900

    HMR server

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import server from './server'
 server.listen(process.env.PORT || 2233)
 
 // TODO: write hot module reloading code here
+if (module.hot) {
+  module.hot.accept('./server', () => {
+    console.log('server hot!!')
+  })
+}
