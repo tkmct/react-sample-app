6641a48494ba04b5001fd643a5d0b5348fe7fd50
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-22 02:36:01 +0900

    clear server hot code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as http from 'http'
 import app from './server'
 
 const server = http.createServer(app)
-let currentApp = app
 
 server.listen(process.env.PORT || 2233, (error: Error) => {
   if (error) {
@@ -11,13 +10,3 @@ server.listen(process.env.PORT || 2233, (error: Error) => {
 
   console.log('🚀 started')
 })
-
-if (module.hot) {
-  // when detecting './server' file update, recreate app
-  module.hot.accept('./server', () => {
-    server.removeListener('request', currentApp)
-    const newApp = require('./server').default
-    server.on('request', newApp)
-    currentApp = newApp
-  })
-}
